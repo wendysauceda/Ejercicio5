c511c8ec249a06824b724cfd25114e9d05f250e3
wendysauceda <86396717+wendysauceda@users.noreply.github.com>
2021-07-02 11:54:02 -0600

    commit_2
    ejercicio_5 Matriz

---
+++ Ejercicio5/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace Ejercicio5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_tamaño = new System.Windows.Forms.TextBox();
+            this.txt_numero = new System.Windows.Forms.TextBox();
+            this.btn_ejecutar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_tamaño
+            // 
+            this.txt_tamaño.Location = new System.Drawing.Point(133, 89);
+            this.txt_tamaño.Name = "txt_tamaño";
+            this.txt_tamaño.Size = new System.Drawing.Size(100, 20);
+            this.txt_tamaño.TabIndex = 0;
+            // 
+            // txt_numero
+            // 
+            this.txt_numero.Location = new System.Drawing.Point(133, 184);
+            this.txt_numero.Name = "txt_numero";
+            this.txt_numero.Size = new System.Drawing.Size(100, 20);
+            this.txt_numero.TabIndex = 1;
+            // 
+            // btn_ejecutar
+            // 
+            this.btn_ejecutar.Location = new System.Drawing.Point(327, 141);
+            this.btn_ejecutar.Name = "btn_ejecutar";
+            this.btn_ejecutar.Size = new System.Drawing.Size(75, 23);
+            this.btn_ejecutar.TabIndex = 2;
+            this.btn_ejecutar.Text = "EJECUTAR";
+            this.btn_ejecutar.UseVisualStyleBackColor = true;
+            this.btn_ejecutar.Click += new System.EventHandler(this.btn_ejecutar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tamaño Matriz:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label2.Location = new System.Drawing.Point(41, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Numeros:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(552, 329);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_ejecutar);
+            this.Controls.Add(this.txt_numero);
+            this.Controls.Add(this.txt_tamaño);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_tamaño;
+        private System.Windows.Forms.TextBox txt_numero;
+        private System.Windows.Forms.Button btn_ejecutar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Ejercicio5/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_ejecutar_Click(object sender, EventArgs e)
+        {
+            int[,] matriz = new int[Convert.ToInt32(txt_tamaño.Text), Convert.ToInt32(txt_numero.Text)];
+            Random matrizaleatoria = new Random();
+
+            for (int fila = 0; fila < matriz.GetLength(0); fila++)
+
+            {
+                for(int columna=0; columna < matriz.GetLength(1); columna++)
+                {
+                    matriz[fila,columna ] = matrizaleatoria.Next(0, 100);
+                    MessageBox.Show("La posicion es:" + fila + "," + columna + "=" + matriz[fila, columna].ToString());
+
+                }
+
+            }
+        }
+    }
+}
